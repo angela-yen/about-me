057d2f3771d164b007744493c4819c397e73c042
Angela Yen <89116466+angela-yen@users.noreply.github.com>
2022-05-17 08:55:12 +1200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ function Footer() {
  
     <Box align='center'>
            <Center p={8}>
-        Made with &nbsp; <Image boxSize='15px' src='/chakraUi.png'></Image> &nbsp;  + &nbsp;  <Image boxSize='15px' src='/logo192.png'></Image> &nbsp; | Angela Yen 2021 &nbsp;  <Image boxSize='15px' src='/logo.png'></Image>
+        Made with &nbsp; <Image boxSize='15px' src='/chakraUi.png'></Image> &nbsp;  + &nbsp;  <Image boxSize='15px' src='/logo192.png'></Image> &nbsp; | Angela Yen 2022 &nbsp;  <Image boxSize='15px' src='/logo.png'></Image>
         </Center>
         
     </Box>
